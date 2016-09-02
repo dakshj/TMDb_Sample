7ef59cad5358f049fa3220e99d9c8bc6569c5b1d
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 02:59:38 +0530

    Set up AppComponent, Inject TmdbApplication into AppComponent

--- app/src/main/java/com/daksh/tmdbsample/app/TmdbApplication.java
+++ app/src/main/java/com/daksh/tmdbsample/app/TmdbApplication.java
@@ -2,6 +2,8 @@ package com.daksh.tmdbsample.app;
 
 import android.app.Application;
 
+import com.daksh.tmdbsample.di.Injector;
+
 /**
  * Created by daksh on 03-Sep-16.
  */
@@ -10,5 +12,7 @@ public class TmdbApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Injector.INSTANCE.initializeAppComponent(this).inject(this);
     }
 }
--- app/src/main/java/com/daksh/tmdbsample/di/AppComponent.java
+++ app/src/main/java/com/daksh/tmdbsample/di/AppComponent.java
@@ -1,7 +1,25 @@
 package com.daksh.tmdbsample.di;
 
+import com.daksh.tmdbsample.app.TmdbApplication;
+import com.daksh.tmdbsample.di.module.AppModule;
+import com.daksh.tmdbsample.di.module.NetworkModule;
+import com.daksh.tmdbsample.di.module.StorageModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * Created by daksh on 03-Sep-16.
  */
+
+@Singleton
+@Component(modules = {
+        AppModule.class,
+        NetworkModule.class,
+        StorageModule.class
+})
 public interface AppComponent {
+
+    void inject(TmdbApplication application);
 }
