3a3651ec17fae829e86e2c0a521880cc62e4a428
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 02:36:37 +0530

    Add singleton instance Injector for Component Injection

---
+++ app/src/main/java/com/daksh/tmdbsample/di/Injector.java
@@ -0,0 +1,29 @@
+package com.daksh.tmdbsample.di;
+
+import com.daksh.tmdbsample.BuildConfig;
+import com.daksh.tmdbsample.app.TmdbApplication;
+import com.daksh.tmdbsample.di.module.AppModule;
+import com.daksh.tmdbsample.di.module.NetworkModule;
+
+/**
+ * Created by daksh on 03-Sep-16.
+ */
+public enum Injector {
+
+    INSTANCE;
+
+    private AppComponent appComponent;
+
+    public AppComponent initializeAppComponent(TmdbApplication application) {
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(application))
+                .networkModule(new NetworkModule(BuildConfig.BASE_URL, BuildConfig.API_KEY))
+                .build();
+
+        return appComponent;
+    }
+
+    public AppComponent getApplicationComponent() {
+        return appComponent;
+    }
+}
