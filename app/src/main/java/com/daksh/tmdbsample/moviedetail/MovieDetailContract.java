aa9e8b54d78fff893f1859d4468f2782e40bdb66
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 03:14:09 +0530

    Add empty MovieDetailContract.java containing View and Presenter for MovieDetail

---
+++ app/src/main/java/com/daksh/tmdbsample/moviedetail/MovieDetailContract.java
@@ -0,0 +1,18 @@
+package com.daksh.tmdbsample.moviedetail;
+
+import com.daksh.tmdbsample.base.BasePresenter;
+import com.daksh.tmdbsample.base.BaseView;
+
+/**
+ * Created by daksh on 03-Sep-16.
+ */
+public interface MovieDetailContract {
+
+    interface View extends BaseView<Presenter> {
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+}
