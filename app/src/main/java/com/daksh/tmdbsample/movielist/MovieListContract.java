db8f3b431ba7ec10b5d3bf7f99b8e4f0eff434d4
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 03:24:34 +0530

    Add preliminary functions to View and Presenter of MovieListContract

--- app/src/main/java/com/daksh/tmdbsample/movielist/MovieListContract.java
+++ app/src/main/java/com/daksh/tmdbsample/movielist/MovieListContract.java
@@ -1,7 +1,12 @@
 package com.daksh.tmdbsample.movielist;
 
+import android.support.annotation.NonNull;
+
 import com.daksh.tmdbsample.base.BasePresenter;
 import com.daksh.tmdbsample.base.BaseView;
+import com.daksh.tmdbsample.data.model.Movie;
+
+import java.util.List;
 
 /**
  * Created by daksh on 03-Sep-16.
@@ -10,9 +15,23 @@ public interface MovieListContract {
 
     interface View extends BaseView<Presenter> {
 
+        void showLoading();
+
+        void showError();
+
+        void showMovies(List<Movie> movies);
+
+        void addMovies(List<Movie> movies);
+
+        void showSortOrderSelector(int currentSortOrder);
     }
 
     interface Presenter extends BasePresenter {
 
+        void loadMovies(Integer page);
+
+        void openMovieDetails(@NonNull Movie movie);
+
+        void setSortOrder(int sortOrder);
     }
 }
