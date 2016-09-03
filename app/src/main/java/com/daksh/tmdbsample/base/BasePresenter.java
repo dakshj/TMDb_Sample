0142d345c1765e3e3f423a22433d32c3de9bb5de
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 13:59:39 +0530

    Add PresenterManager which internally uses Guava's Cache
    PresenterManager will make handling configuration changes easier,
    so that the Presenter for a View is not instantiated every time
    the View (Activity, Fragment, etc.) goes through a restart, after, say,
    rotation.

--- app/src/main/java/com/daksh/tmdbsample/base/BasePresenter.java
+++ app/src/main/java/com/daksh/tmdbsample/base/BasePresenter.java
@@ -1,11 +1,50 @@
 package com.daksh.tmdbsample.base;
 
+import android.support.annotation.NonNull;
+
+import java.lang.ref.WeakReference;
+
 /**
  * Created by daksh on 03-Sep-16.
  */
-public interface BasePresenter {
+public abstract class BasePresenter<M, V> {
+    protected M model;
+    private WeakReference<V> view;
 
-    void start();
+    public void setModel(M model) {
+        resetState();
+        this.model = model;
+        if (setupDone()) {
+            updateView();
+        }
+    }
 
-    void goBack();
+    protected void resetState() {
+    }
+
+    public void bindView(@NonNull V view) {
+        this.view = new WeakReference<>(view);
+        if (setupDone()) {
+            updateView();
+        }
+    }
+
+    public void unbindView() {
+        this.view = null;
+    }
+
+    protected V view() {
+        if (view == null) {
+            return null;
+        } else {
+            return view.get();
+        }
+    }
+
+    protected abstract void updateView();
+
+    protected boolean setupDone() {
+        return view() != null && model != null;
+    }
 }
+
