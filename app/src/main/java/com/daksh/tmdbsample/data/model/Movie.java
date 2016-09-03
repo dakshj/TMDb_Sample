f5a9d1039e76354bde8d06998dbdbbb5c1584696
Daksh Jotwani <daksh.jotwani@zimmber.com>
2016-09-03 14:36:07 +0530

    Make Movie model implement Parcelable

--- app/src/main/java/com/daksh/tmdbsample/data/model/Movie.java
+++ app/src/main/java/com/daksh/tmdbsample/data/model/Movie.java
@@ -1,5 +1,8 @@
 package com.daksh.tmdbsample.data.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
@@ -7,19 +10,26 @@ import java.util.Date;
 /**
  * Created by daksh on 03-Sep-16.
  */
-public class Movie {
+public class Movie implements Parcelable {
 
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
     private final String title;
-
     @SerializedName("poster_path")
     private final String imageUrl;
-
     @SerializedName("overview")
     private final String synopsis;
-
     @SerializedName("vote_average")
     private final double userRating;
-
     private final Date releaseDate;
 
     public Movie(String title, String imageUrl, String synopsis, double userRating, Date releaseDate) {
@@ -28,6 +38,15 @@ public class Movie {
         this.synopsis = synopsis;
         this.userRating = userRating;
         this.releaseDate = releaseDate;
+    }
+
+    protected Movie(Parcel in) {
+        this.title = in.readString();
+        this.imageUrl = in.readString();
+        this.synopsis = in.readString();
+        this.userRating = in.readDouble();
+        long tmpReleaseDate = in.readLong();
+        this.releaseDate = tmpReleaseDate == -1 ? null : new Date(tmpReleaseDate);
     }
 
     public String getTitle() {
@@ -48,5 +67,19 @@ public class Movie {
 
     public Date getReleaseDate() {
         return releaseDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.imageUrl);
+        dest.writeString(this.synopsis);
+        dest.writeDouble(this.userRating);
+        dest.writeLong(this.releaseDate != null ? this.releaseDate.getTime() : -1);
     }
 }
